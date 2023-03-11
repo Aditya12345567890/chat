35def38c9e7ff628e989b2d73d4d214fee0bfd15
Aditya <aditya2naresh@gmail.com>
2023-03-11 16:20:11 -0500

    commit

--- server.js
+++ server.js
@@ -2,7 +2,22 @@
 const express = require("express");
 const port = 1234;
 let app =express();
+const socket_io = require("socket.io");
 app.set('view engine', 'ejs')
+
+var io = socket.io();
+app.io =io;
+
+io.on('connection', function(socketUser){
+  socketUser.on('new msg', function(msg){
+    io.emit('msg',msg)
+  })
+})
+
+app.post('/chat', function(req,res,next){
+  let name=req.body.name;
+  res.render('chat',{name});
+})
 
 app.get("/",function (req,res){
     res.render("index")
